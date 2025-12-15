cfe03b4ae2e00fb7d4844385d41e3bfd6fe5fca0
durcevicjuraj <jurajdurcevic11@gmail.com>
2025-12-15 12:33:38 +0100

    Added the vite-plugin-qrcode

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    qrcode(),
   ],
 })
